4271d78201526c1c3c74df86ddac05d7a4664618
Gitros <31899790+Gitros@users.noreply.github.com>
2024-01-12 14:14:39 +0100

    management project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import NewProject from "./components/NewProject";
+import Sidebar from "./components/ProjectsSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar />
+      <NewProject />
+      <h1 className="my-8 text-center text-5xl font-bold">Learning React</h1>
+    </main>
   );
 }
 
